0ff664ebf9f449134d64de023300ab4e41caff77
John Sadie <John Sadie>
2016-12-08 23:49:07 -0700

    affine transformation

--- GLES2StarterKit-2.0/ezview.c
+++ GLES2StarterKit-2.0/ezview.c
@@ -64,19 +64,19 @@ int read_scene(char* filename){
     /*Error Checking*/
     if(fh == 0){
         fclose(fh);
-      //  fprintf(stderr, "ERROR! The file: '%s' does not exist...\n", filename);
+        fprintf(stderr, "ERROR! The file: '%s' does not exist...\n", filename);
         exit(1);
     }
     c = fgetc(fh);
     if (c!= 'P'){
-      //  fprintf(stderr, "ERROR! Not a ppm file...\n");
+        fprintf(stderr, "ERROR! Not a ppm file...\n");
         exit(1);
     }
     ungetc(c, fh);
     //Allocating memory
      pixelImg = (Scene*)malloc(sizeof(Scene));
     if(pixelImg == 0){
-      //  fprintf(stderr, "ERROR! Can't allocate memory... \n");
+        fprintf(stderr, "ERROR! Can't allocate memory... \n");
         exit(1);
     }
     c = fgetc(fh);
@@ -115,6 +115,7 @@ int read_scene(char* filename){
     fclose(fh);
     return 0;
 }
+
 
 /*texdemo.c*/
 /*Provided by Dr. Palmer*/
@@ -220,24 +221,154 @@ void glCompileShaderOrDie(GLuint shader) {
   }
 }
 
+int main(int argc, char *argv[])
+{
+  GLFWwindow* window;
+  GLuint vertex_buffer, vertex_shader, fragment_shader, program, index;
+  GLint mvp_location, vpos_location, vcol_location;
+  glfwSetErrorCallback(error_callback);
 
-int main(int argc, char *argv[]){
-    int c;
+  //Checks if the user has provided the correct command line arguments
+  if(argc != 2){
+    fprintf(stderr, "ERROR! Enter the ppm file name with correct extension... \n");
+    exit(1);
+  }
+  char *filename = argv[1];
+  read_scene(filename);
 
-    pixelImage *image;
-      //Check if arguments are not equal to 2
-       if(argc != 2){
-        fprintf(stderr, "ERROR\n");
-        return 1;
-    }
-    FILE *fp;
-    fp = fopen(inputType, "r");
-    if(fp == 0){
-        fp = fopen(inputType, "rb");
-    }
-    fgetc(fp);
-    c = fgetc(fp);
-    ungetc(c, fp);
+  if (!glfwInit())
+      exit(EXIT_FAILURE);
 
+      glfwDefaultWindowHints();
+      glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
+      glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
+  window = glfwCreateWindow(300, 300, "EZ VIEW", NULL, NULL);
+  if (!window)
+  {
+      glfwTerminate();
+      exit(EXIT_FAILURE);
+  }
+
+  glfwSetKeyCallback(window, key_callback);
+
+  glfwMakeContextCurrent(window);
+  // gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+  glfwSwapInterval(1);
+
+  // NOTE: OpenGL error checks have been omitted for brevity
+
+  glGenBuffers(1, &vertex_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertexes), vertexes, GL_STATIC_DRAW);
+
+  vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+  glCompileShaderOrDie(vertex_shader);
+
+  fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+  glCompileShaderOrDie(fragment_shader);
+
+  program = glCreateProgram();
+  glAttachShader(program, vertex_shader);
+  glAttachShader(program, fragment_shader);
+  glLinkProgram(program);
+  // more error checking! glLinkProgramOrDie!
+
+  mvp_location = glGetUniformLocation(program, "MVP");
+  assert(mvp_location != -1);
+
+  vpos_location = glGetAttribLocation(program, "vPos");
+  assert(vpos_location != -1);
+
+  GLint texcoord_location = glGetAttribLocation(program, "TexCoordIn");
+  assert(texcoord_location != -1);
+
+  GLint tex_location = glGetUniformLocation(program, "Texture");
+  assert(tex_location != -1);
+
+  glEnableVertexAttribArray(vpos_location);
+  glVertexAttribPointer(vpos_location,
+  	  2,
+  	  GL_FLOAT,
+  	  GL_FALSE,
+      sizeof(Vertex),
+  	  (void*) 0);
+
+  glEnableVertexAttribArray(texcoord_location);
+  glVertexAttribPointer(texcoord_location,
+  	  2,
+  	  GL_FLOAT,
+  	  GL_FALSE,
+      sizeof(Vertex),
+  	  (void*) (sizeof(float) * 2));
+
+  GLuint texID;
+  glGenTextures(1, &texID);
+  glBindTexture(GL_TEXTURE_2D, texID);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, pixelImg->width, pixelImg->height, 0, GL_RGB,
+   GL_UNSIGNED_BYTE, pixelImg->PixelBuffer);
+
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texID);
+  glUniform1i(tex_location, 0);
+
+  while (!glfwWindowShouldClose(window))
+  {
+      float ratio;
+      int width, height;
+      mat4x4 m, p, mvp;
+      mat4x4 rotation, translation, scaling, shearing, arm;
+
+      glfwGetFramebufferSize(window, &width, &height);
+      ratio = width / (float) height;
+
+      glViewport(0, 0, width, height);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      //Creates an identity matrix
+      //for our affine transformation
+      mat4x4_identity(m);
+      mat4x4_identity(rotation);
+      mat4x4_identity(translation);
+      mat4x4_identity(scaling);
+      mat4x4_identity(shearing);
+
+      //Acquiring the data for transformation
+      mat4x4_rotate_Z(rotation, rotation, rotate);
+      mat4x4_translate(translation,xTrans,yTrans,zTrans);
+      mat4x4_scale_aniso(scaling, scaling, scale, scale, scale);
+      mat4x4_shear(shearing, shearing, shear, shear);
+      mat4x4_add(m,translation,m);
+      mat4x4_add(m,scaling,m);
+      mat4x4_add(m,shearing,m);
+      mat4x4_mul(m,rotation,m);
+
+      mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+      mat4x4_mul(mvp, p, m);
+      /*
+      mat4x4_identity(m);
+      mat4x4_rotate_Z(m, m, (float) glfwGetTime());
+      mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+      mat4x4_mul(mvp, p, m);
+      */
+
+      glUseProgram(program);
+      glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
+      glDrawArrays(GL_TRIANGLES, 0, 6);
+
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
 }
